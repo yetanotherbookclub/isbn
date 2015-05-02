f2e87a43bb61babd7f9880de088b228aa23ded60
Matthew Werner <mwerner@zendesk.com>
2015-05-01 17:24:29 -0700

    add root route

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,13 @@
 require './lib/book_api'
 
 class BooksController < ApplicationController
+  def index
+    render json: {
+      '/:isbn' => 'Lookup an isbn',
+      '/search/:term' => 'Do a search for a book'
+    }.to_json
+  end
+
   def show
     render json: client.book.to_json
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'search/:id' => 'books#search'
   resources :books, only: [:show], path: ""
+  root 'books#index'
 end
